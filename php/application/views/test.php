da688d20aeb11d2f639704e7acce819f004107a8
XuanDing <xdb57@mail.missouri.edu>
2015-03-17 00:20:05 -0500

    test my view7

---
+++ php/application/views/test.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+<title> Welcome, this is just a test </title>
+<body>
+	<h1>BBBBBBBB</h1>
+</body>
+</head>
+</html>
---
+++ php/application/views/test2.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+<title> Welcome, this is just a test </title>
+<body>
+	<h1>3333333</h1>
+</body>
+</head>
+</html>
