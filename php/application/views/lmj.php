206db737a894159148eeba99e6b3edac39b91841
XuanDing <xdb57@mail.missouri.edu>
2015-03-18 22:11:42 -0500

    for fun3

--- php/application/views/lmj.php
+++ php/application/views/lmj.php
@@ -4,7 +4,7 @@
 <title> Welcome, this page belongs to LMJ </title>
 <body>
 	<h1>LMJ,This is your personal page!</h1>
-	<img src = "https://www.facebook.com/photo.php?fbid=1499667666928382&set=a.1381767275385089.1073741825.100006554886764&type=1" alt = "" style = "width: 200px; height: 200 px">
+	<img src = "http://www.supermanhomepage.com/images/logos-emblems/s-logo.gif" alt = "" style = "width: 200px; height: 200 px">
 </body>
 </head>
 </html>
