31980da9749cd8a6dde5ef48a2feb39d125aef10
XuanDing <xdb57@mail.missouri.edu>
2015-03-18 22:56:35 -0500

    1

--- php/application/views/lmj.php
+++ php/application/views/lmj.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-<title> Welcome, this page belongs to LMJ </title>
+<title> 大家好我是刘美君 </title>
 <style>
 
 /*!
@@ -201,12 +201,12 @@ section.codeigniter p {
 	margin-bottom: 1em;
 }
 </style>
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />`
 </head>
 <body>
-	<h1>LMJ,This is your personal page!</h1>
+	<h1>当然啦，我是刘美君</h1>
 	<a href = "https://www.facebook.com/profile.php?id=100006554886764&fref=ts">
 	<img src = "https://scontent-ord.xx.fbcdn.net/hphotos-xap1/v/t1.0-9/10304438_1499667666928382_2781094475537773514_n.jpg?oh=ce96f5cef069dcaaff77a756502bd910&oe=55B0DD3C" alt = "" style = "width: 400px; height: 400 px">
 	</a>
-	<img src = "imgs/IMG_0372.jpg" alt = "" width = "400" height = "400">
 </body>
 </html>
