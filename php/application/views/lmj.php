0fdb9841deb9a057dc0afa5c353607b5bf983f6e
XuanDing <xdb57@mail.missouri.edu>
2015-03-18 22:07:27 -0500

    for fun3

--- php/application/views/lmj.php
+++ php/application/views/lmj.php
@@ -4,6 +4,7 @@
 <title> Welcome, this page belongs to LMJ </title>
 <body>
 	<h1>LMJ,This is your personal page!</h1>
+	<img src = "IMG_0372.jpg" alt = "" style = "width: 200px; height: 200 px">
 </body>
 </head>
 </html>
