625ffb8dab92544867577503434adc0f21c8657d
XuanDing <xdb57@mail.missouri.edu>
2015-03-18 22:19:37 -0500

    1

--- php/application/views/lmj.php
+++ php/application/views/lmj.php
@@ -4,7 +4,10 @@
 <title> Welcome, this page belongs to LMJ </title>
 <body>
 	<h1>LMJ,This is your personal page!</h1>
+	<a href = "https://www.facebook.com/profile.php?id=100006554886764&fref=ts">
 	<img src = "https://scontent-ord.xx.fbcdn.net/hphotos-xap1/v/t1.0-9/10304438_1499667666928382_2781094475537773514_n.jpg?oh=ce96f5cef069dcaaff77a756502bd910&oe=55B0DD3C" alt = "" style = "width: 400px; height: 400 px">
+	</a>
+	<img src = "IMG_0372.jpg" alt = "" width = "400" height = "400">
 </body>
 </head>
 </html>
