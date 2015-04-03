89e074b54b5c1a3390c461ccfd7c952bf7ba2e6a
Andrew Pistole <aapcnc@mail.missouri.edu>
2015-04-03 12:21:56 -0500

    fixed redirect

--- php/application/views/welcome_message.php
+++ php/application/views/welcome_message.php
@@ -275,7 +275,7 @@ $ git push</pre>
 
 
         <footer>
-          <div class="logo"><a /*href="https://www.openshift.com/"*/ href = "https://php-sweteame.rhcloud.com/loginPHP></a></div>
+          <div class="logo"><a href=""https://php-sweteame.rhcloud.com/loginPHP"></a></div>
         </footer>
 </section>
 
