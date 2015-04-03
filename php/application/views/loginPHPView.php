0c9ccaa7f704a9ed5a729485efc2b85b8fcbfef5
XuanDing <xdb57@mail.missouri.edu>
2015-04-03 14:12:24 -0500

    no message

--- php/application/views/loginPHPView.php
+++ php/application/views/loginPHPView.php
@@ -2,11 +2,12 @@
 <html>
 <head>
 <meta charset=UTF-8>
+<titel>Welcome to apply TA/PLA</title>
 </head>
 <body>
 	<div align = "center">
 		<div id ="login">
-			<p>Please login
+			<p>Please login</p>
 			<form action = "/~aapcnc/cs3380/lab8/index.php" method='post'>
 				<label for="username">username:</label>
 				<input type="text" name="username" id="username">
