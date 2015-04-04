29b8029c7463c81cb4b8f391a5288aa34d470012
Alex Drake <amd2b7@mail.misouri.edu>
2015-04-04 11:08:11 -0500

    Added some more git stuff, hopefully helps keeping everything going smoothly

--- php/application/views/welcome_message.php
+++ php/application/views/welcome_message.php
@@ -279,10 +279,15 @@ To sit on my throne as the Prince of Bel Air </u></p>
 # Commit your changes and push to OpenShift
 
 $ git commit -a -m 'Some commit message'
-$ git push
 DO NOT PUSH YOUR MASTER BRANCH
 $ git push -u origin <branch name>
-The above command should take whatever local branch you are in and push it to the remote repo and create a remote branch.</pre>
+The above command should take whatever local branch you are in and push it to the remote repo and create a remote branch.
+$ git branch -a
+This will show all branches associated with the repo. Your local branches will be on top, remote branches will be on bottom.
+$ git merge origin 
+should never be ran or even typed without the groups approval. All merges into the remote master will be done by logging into the application itself. It does have its own ssh shell.
+$ git merge
+might be used, not sure on the symbolic link stuff. Might be ok for you to merge your local branches into your local master if we can set up that whole dev1/dev2/dev3 personal testing space he was talking about. Still not ok to push this.</pre>
 
 
                   <ul>
