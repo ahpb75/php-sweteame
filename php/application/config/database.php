445059ac393016cac30f0830ca418addbd3289f8
XuanDing <xdb57@mail.missouri.edu>
2015-04-05 15:09:29 -0500

    Database connection success.

--- php/application/config/database.php
+++ php/application/config/database.php
@@ -50,9 +50,9 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = getenv('OPENSHIFT_MYSQL_DB_HOST');
 $db['default']['port']     = getenv('OPENSHIFT_MYSQL_DB_PORT');
-$db['default']['username'] = getenv('adminclX39uA');
-$db['default']['password'] = getenv('Mylsa_YCHshg');
-$db['default']['database'] = getenv('webapp');
+$db['default']['username'] = 'adminclX39uA';
+$db['default']['password'] = 'MyIsa_YCHshg';
+$db['default']['database'] = 'webapp';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
