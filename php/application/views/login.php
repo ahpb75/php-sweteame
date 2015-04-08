67b0ba25bc861f715997bfa949c9b352b928bc19
XuanDing <xdb57@mail.missouri.edu>
2015-04-07 19:32:59 -0500

    no message

--- php/application/views/login.php
+++ php/application/views/login.php
@@ -5,7 +5,7 @@
 <div align = "center">
 <title>Welcome to apply TA/PLA</title>
 </div>
-<link rel="stylesheet" type="text/css" href="<?php base_url();?>style.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>style.css" />
 </head>
 <body>
 	<div align = "center">
