ce496f07391026362d56795172db939412782818
XuanDing <xdb57@mail.missouri.edu>
2015-04-09 12:21:58 -0500

    minor database error

--- php/application/config/database.php
+++ php/application/config/database.php
@@ -50,9 +50,9 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = getenv('OPENSHIFT_MYSQL_DB_HOST');
 $db['default']['port']     = getenv('OPENSHIFT_MYSQL_DB_PORT');
-$db['default']['username'] = getenv('adminclX39uA');
-$db['default']['password'] = getenv('sweteame2015');
-$db['default']['database'] = getenv('webapp');
+$db['default']['username'] = 'adminclX39uA';
+$db['default']['password'] = 'sweteame2015';
+$db['default']['database'] = 'webapp';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
