aefcf87eb33a29db83c4f7bdf9d4d7c8ca74ac4c
XuanDing <xdb57@mail.missouri.edu>
2015-04-09 11:26:00 -0500

    no message

--- php/application/models/login_model.php
+++ php/application/models/login_model.php
@@ -9,21 +9,21 @@ class Login_model extends CI_Model {
     //     $data=array('username' => $this->input->post('username'),'password' => md5($this->input->post('password')),'permissions' => $this->input->post('permissions'));
     //     $this->db->insert('User',$data);
     // }
-    function login()
-    {
-        $query = $this->db->get_where('User',array('username' => $this->input->post('username'),'password' => $this->input->post('password'));
+    // function login()
+    // {
+    //     $query = $this->db->get_where('User',array('username' => $this->input->post('username'),'password' => $this->input->post('password'));
 
-        if($query->num_rows()>0)
-        {
-            foreach($query->result() as $key => $row)
-            {
-                $newdata = array('user_name' => $row->username,'user_type' => $row->permissions);
-            }
+    //     if($query->num_rows()>0)
+    //     {
+    //         foreach($query->result() as $key => $row)
+    //         {
+    //             $newdata = array('user_name' => $row->username,'user_type' => $row->permissions);
+    //         }
 
-            $this->session->set_userdata($newdata);
-            return TRUE;
-        }
-        return FALSE;
-    }
+    //         $this->session->set_userdata($newdata);
+    //         return TRUE;
+    //     }
+    //     return FALSE;
+    // }
 }
 ?>
