2bc97d3bc30429dfa2ab092f97dae4c4dc6049b7
XuanDing <xdb57@mail.missouri.edu>
2015-04-09 00:07:56 -0500

    no message

--- php/application/views/test.php
+++ php/application/views/test.php
@@ -1,12 +1,5 @@
-<html>
-<head>
-<title><?php echo $this->input->post('username');
+<?php echo $this->input->post('username');
 			echo "<br>";
             echo md5($this->input->post('password'));
             echo "<br>";
             echo $this->input->post('permissions');?>
-</title>
-</head>
-<body>
-</body>
-</html>
