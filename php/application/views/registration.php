537ff56d3b58d5ee070c037191800a0295fa2735
XuanDing <xdb57@mail.missouri.edu>
2015-04-08 23:55:07 -0500

    no message

--- php/application/views/registration.php
+++ php/application/views/registration.php
@@ -9,25 +9,25 @@
 </head>
 <body>
 	<div align = "center">
-		<div id ="login">
+		<div id ="registration">
 			<h1>Please Register</h1>
 				<?php
-				echo form_open("welcome/registration");
+				echo form_open("welcome/test");
 				?>
 				<label for="username">Username:</label>
-				<input type="text" name="username" id="username" value="<?php echo set_value('username')?>;">
+				<input type="text" name="username" id="username" value="<?php echo set_value('username');?>" />
 				<br>
 				<label for="password">Password:</label>
-				<input type="password" name="password" id="password" value="<?php echo set_value('password')?>;">
+				<input type="password" name="password" id="password" value="<?php echo set_value('password');?>" />
 				<br>
 				<label for="password">Confirm Password:</label>
-				<input type="password" name="cpassword" id="password" value="<?php echo set_value('cpassword')?>;">
+				<input type="password" name="cpassword" id="password" value="<?php echo set_value('cpassword');?>" />
 				<br>
-				<input type="radio" name="permissions" value="2" checked>Applicant
+				<input type="radio" name="permissions" value="2" checked>Applicant />
 				<br>
-				<input type="radio" name="permissions" value="1">Staff
+				<input type="radio" name="permissions" value="1">Staff />
 				<br>
-				<input type="submit" name="submit" value="submit">
+				<input type="submit" name="submit" value="submit" />
 				<?php
 				echo form_close();
 				?>
