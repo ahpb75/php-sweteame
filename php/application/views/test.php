05eaf55c862e8c3d264b603c76506e52452fa074
XuanDing <xdb57@mail.missouri.edu>
2015-04-08 22:56:09 -0500

    no message

--- php/application/views/test.php
+++ php/application/views/test.php
@@ -1,3 +1,8 @@
-<?php
-	echo $username;
-?>
+<html>
+<head>
+<title><?php echo $user_name;?></title>
+</head>
+<body>
+	<h1>#<?php echo $this->session->userdata('user_name');?></h1>
+</body>
+</html>
