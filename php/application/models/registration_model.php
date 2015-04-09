ec9efdf0f05e7f3180372d5f116a8f1b7704694a
XuanDing <xdb57@mail.missouri.edu>
2015-04-09 10:20:11 -0500

    no message

--- php/application/models/registration_model.php
+++ php/application/models/registration_model.php
@@ -1,17 +1,13 @@
 <?php
-class User_model extends CI_Model {
+class Registration_model extends CI_Model {
     function __construct()
     {
         parent::__construct();
     }
     function add_user()
     {
-        $data=array(
-            'username' => $this->input->post('username'),
-            'password' => md5($this->input->post('password'))
-            // 'permissions' => $this->input->post('permissions')
-            );
-            $this->db->insert('User',$data);
+        $data=array('username' => $this->input->post('username'),'password' => $this->input->post('password'));
+        $this->db->insert('User',$data);
     }
 }
 ?>
