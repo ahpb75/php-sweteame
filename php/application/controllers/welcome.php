8a302d66f1a58ff743375893737620f39c2ad8c0
XuanDing <xdb57@mail.missouri.edu>
2015-04-09 10:54:20 -0500

    no message

--- php/application/controllers/welcome.php
+++ php/application/controllers/welcome.php
@@ -69,34 +69,34 @@ class Welcome extends CI_Controller {
 			}
 		}
 	}
-	public function login()
-	{
-		$this->load->model('user_model');
-		// if($this->session->userdata('user_name') != "")
-		// 	$this->load->view('login');
-		// else{
-			$this->load->library('form_validation');
-			$this->form_validation->set_rules('username','Username','required');
-			$this->form_validation->set_rules('password','Password','required');
+	// public function login()
+	// {
+	// 	$this->load->model('user_model');
+	// 	// if($this->session->userdata('user_name') != "")
+	// 	// 	$this->load->view('login');
+	// 	// else{
+	// 		$this->load->library('form_validation');
+	// 		$this->form_validation->set_rules('username','Username','required');
+	// 		$this->form_validation->set_rules('password','Password','required');
 
-			if($this->form_validation->run() == FALSE)
-			{
-				$this->error();
-			}
-			else
-			{
-				$result = $this->user_model->login();
-				if($result)
-				{
-					$this->index();
-				}
-				else
-				{
-					$this->error();
-				}
-			}
-		// }
-	}
+	// 		if($this->form_validation->run() == FALSE)
+	// 		{
+	// 			$this->error();
+	// 		}
+	// 		else
+	// 		{
+	// 			$result = $this->user_model->login();
+	// 			if($result)
+	// 			{
+	// 				$this->index();
+	// 			}
+	// 			else
+	// 			{
+	// 				$this->error();
+	// 			}
+	// 		}
+	// 	// }
+	// }
 	public function test()
 	{
 		$this->load->view('test');
