76edb987e270dc686e7340f7f469d7786ed472c3
unknown <peth.andrew@gmail.com>
2015-04-08 21:06:19 -0500

    Made functions public in form_model.php

--- php/application/models/Form_model.php
+++ php/application/models/Form_model.php
@@ -6,11 +6,11 @@
  * Time: 3:26 PM
  */
 class Form_model extends CI_Model {
-    function __construct()
+    public function __construct()
     {
         parent::__construct();
     }
-    function SaveForm($form_data)
+    public function SaveForm($form_data)
     {
         $this->db->insert('Application', $form_data);
         if ($this->db->affected_rows() == '1')
