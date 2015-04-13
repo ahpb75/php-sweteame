ad35daeac890dc9ee979d6c5bf598a8cf98183b3
Chantal <cflzr3@mail.missouri.edu>
2015-04-13 18:55:31 -0500

    Figuring logic on re-direct

---
+++ php/application/views/Instructor_view.php
@@ -0,0 +1,57 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<!DOCTYPE HTML>
+<html>
+<head>
+    <html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title> TA Selection </title>
+
+<!-- Bootstrap Core CSS -->
+    <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../../bootstrap/css/heroic-features.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+    <title> TA Selection </title>
+
+<body>
+<h1> Computer Science Department </h1>
+<h2> Applicants </h2>
+
+<?php
+echo "hi";
+foreach($applicants as $data){
+
+    echo "hi";
+    echo $data; ?>
+
+    <br />;
+
+
+
+    <button onclick="location.href ='Need location of function that pulls out form';" id = "view_button"> View Application</button>
+    <!--Will redirect to another page with application that they can select to leave comments -->
+}
+
+
+</body>
+
+</html>
