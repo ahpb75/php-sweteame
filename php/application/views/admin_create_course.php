7acfa16be5e9059610c6f315774f2a026b4d51a6
XuanDing <xdb57@mail.missouri.edu>
2015-04-16 02:20:58 -0500

    no message

--- php/application/views/admin_create_course.php
+++ php/application/views/admin_create_course.php
@@ -11,14 +11,12 @@
 	<div align = "center">
 		<div id ="login">
 			<h1>Create Course</h1>
-				<?php echo form_open("admin_controller/create_course");?>
-				<label for="Coursename">Coursename:</label>
-				<input type="text" name="Coursename" id="Coursename" value = "<?php echo set_value('Coursename');?>" />
-				<label for="CourseId">CourseId:</label>
-				<input type="text" name="CourseId" id="CourseId" value = "<?php echo set_value('CourseId');?>" />
-				<br>
-				<input type="submit" name="submit" value="submit" />
-				<?php echo form_close();?>
+				<?php 
+				echo form_open("admin_controller/create_course");
+				echo form_input('CourseId','');
+				echo form_input('Coursename','');
+				echo form_submit('submit','submit');
+				echo form_close();?>
 			</p>
 			</div>
 </div>
