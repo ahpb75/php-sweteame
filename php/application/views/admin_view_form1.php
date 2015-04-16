96af2cc87e5b9b75af8427408490e33944462f3a
XuanDing <xdb57@mail.missouri.edu>
2015-04-16 00:39:55 -0500

    no message

--- php/application/views/admin_view_form1.php
+++ php/application/views/admin_view_form1.php
@@ -2,15 +2,13 @@
 <head></head>
 <body>
 	<div align = "center">
-		<?php
-		foreach($username as $value)
-		echo form_open('admin_controller/view_form2');
-		echo $value;
-		echo form_submit('view','view this applicant');
-		echo form_close();
-		echo "<br>";
-		echo endforeach;
-		?>
+		<ul>
+<?php foreach ($username as $item);?>
+
+<li><?php echo $item;?></li>
+
+<?php endforeach;?>
+</ul>
 	</div>
 </body>
 </html>
