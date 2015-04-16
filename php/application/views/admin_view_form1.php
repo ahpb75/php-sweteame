e5dbf0894790069eb1c71521f7b08885adb9749a
XuanDing <xdb57@mail.missouri.edu>
2015-04-16 01:07:44 -0500

    no message

--- php/application/views/admin_view_form1.php
+++ php/application/views/admin_view_form1.php
@@ -3,7 +3,10 @@
 <body>
 <ul>
 <?php foreach ($username as $item):?>
-<li><?php echo $item; echo form_open('Admin_controller/view_form2'); echo form_submit('view','view this applicant'); echo form_close();?></li>
+<li><?php echo form_open('Admin_controller/view_form2');
+	 echo form_input('username',$item);
+	 echo form_submit('view','view this applicant');
+	 echo form_close();?></li>
 <?php endforeach;?>
 </ul>
 </body>
