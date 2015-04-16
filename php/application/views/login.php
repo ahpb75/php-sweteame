480a6be6fba4c96dff514c3a4b2f5c961aed51e9
vincentlinuno <vincentlinuno@yahoo.com>
2015-04-16 18:40:00 -0500

    made login UI

--- php/application/views/login.php
+++ php/application/views/login.php
@@ -1,29 +1,49 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset=UTF-8>
-<div align = "center">
-<title>Welcome to apply TA/PLA</title>
-</div>
-<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>bootstrap/css/bootstrap.css" />
+<head lang="en">
+    <meta charset="UTF-8">
+    <link href="../../bootstrap/css/login.css" rel="stylesheet">
+    <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Please Log In</title>
 </head>
 <body>
-	<div align = "center">
-		<div id ="login">
-			<h1>Please login</h1>
-				<?php if ($login == 'FALSE')
-						echo "<p> Your username or passowrd was incorrect.</p>"?>
-				<?php echo form_open("welcome/login");?>
-				<label for="username">username:</label>
-				<input type="text" name="username" id="username" value = "<?php echo set_value('username');?>" />
-				<label for="password">password:</label>
-				<input type="password" name="password" id="password" value = "<?php echo set_value('password');?>" />
-				<br>
-				<input type="submit" name="submit" value="submit" />
-				<?php echo form_close();?>
-			<p>Register <a href=<?php echo site_url("welcome/registration");?>>here</a></p>
-			</p>
-			</div>
-</div>
+<div class="container">
+
+    <div class="row" id="pwd-container">
+        <div class="col-md-4"></div>
+
+        <div class="col-md-4">
+            <section class="login-form">
+                <form method="post" action="#" role="login">
+                    <img src="../../bootstrap/img/mu_logo.png" class="img-responsive" alt="" />
+                    <center>
+                        <?php if ($login == 'FALSE')
+                            echo "<p> Your username or password was incorrect.</p>"?>
+                        <?php echo form_open("welcome/login");?>
+                    </center>
+                    <input type="text" id="username" name="username" placeholder="Username" required class="form-control input-lg" value="<?php echo set_value('username');?>" />
+
+                    <input type="password" class="form-control input-lg" id="password" name="password" placeholder="Password" required="" value = "<?php echo set_value('password');?>"/>
+
+                    <button type="submit" name="submit" value="submit" class="btn btn-lg btn-primary btn-block">Sign in</button>
+                    <?php echo form_close();?>
+                    <div>
+                        <a href=<?php echo site_url("welcome/registration");?>Create account</a>
+                    </div>
+
+                </form>
+
+                <div class="form-links">
+                    <a href=<?php echo site_url("welcome/home");?>Home Page</a>
+                </div>
+            </section>
+        </div>
+
+        <div class="col-md-4"></div>
+
+
+    </div>
+
 </body>
 </html>
