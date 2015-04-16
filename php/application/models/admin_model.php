8a3043525916b43a86e60dbcf9db1bf34ff63274
XuanDing <xdb57@mail.missouri.edu>
2015-04-15 22:17:33 -0500

    no message

--- php/application/models/admin_model.php
+++ php/application/models/admin_model.php
@@ -12,7 +12,27 @@ class Admin_model extends CI_Model {
     function view_form()
     {
         $query = $this->db->get('Application');
-        return $query->result();
+        // return $query->result();
+        $form = array()
+        $form['GPA'] = $query->result()->GPA;
     }
+     // $form_data = array(
+     //            'position' => @$this->input->post('position'),
+     //            'GPA' => @$this->input->post('GPA'),
+     //            'program_level' => @$this->input->post('program_level'),
+     //            'graduate_program' => @$this->input->post('graduate_program'),
+     //            'advisor' => @$this->input->post('advisor'),
+     //            'phone' => @$this->input->post('phone'),
+     //            'email' => @$this->input->post('email'),
+     //            'gradDate' => @$this->input->post('gradDate'),
+     //            'optScore' => @$this->input->post('optScore'),
+     //            'optWhen' => @$this->input->post('optWhen'),
+     //            'GATO' => @$this->input->post('GATO'),
+     //            'SPEAK' => @$this->input->post('SPEAK'),
+     //            'SPEAKdate' => @$this->input->post('SPEAKdate'),
+     //            'ONITA' => @$this->input->post('ONITA'),
+     //            'ONITA_date' => @$this->input->post('ONITA_date')
+     //        );
+
 }
 ?>
