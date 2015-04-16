da37525b9de98af123c47898866d8e0c415ae85c
XuanDing <xdb57@mail.missouri.edu>
2015-04-15 21:56:06 -0500

    no message

--- php/application/controllers/Admin_controller.php
+++ php/application/controllers/Admin_controller.php
@@ -20,7 +20,7 @@ class Admin_controller extends CI_Controller {
 	public function __construct() 
 	{
 		parent::__construct();
-		$this->load->model('admin_model')
+		$this->load->model('admin_model');
 	}
 
 // 	public function index(){
--- php/application/models/admin_model.php
+++ php/application/models/admin_model.php
@@ -9,21 +9,10 @@ class Admin_model extends CI_Model {
     //     $data=array('username' => $this->input->post('username'),'password' => md5($this->input->post('password')),'permissions' => $this->input->post('permissions'));
     //     $this->db->insert('User',$data);
     // }
-    function login()
+    function view_form()
     {
-        $query = $this->db->get_where('User',array('username' => $this->input->post('username'),'password' => md5($this->input->post('password'))));
-
-        if($query->num_rows()>0)
-        {
-            foreach($query->result() as $key => $row)
-            {
-                $newdata = array('user_name' => $row->username,'user_type' => $row->permissions);
-            }
-
-            $this->session->set_userdata($newdata);
-            return TRUE;
-        }
-        return FALSE;
+        $query = $this->db->get('Application');
+        return $query;
     }
 }
 ?>
