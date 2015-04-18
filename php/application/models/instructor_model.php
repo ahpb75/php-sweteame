398908869b9af83f42e23ee4e322c5c7524924bf
Alex Drake <amd2b7@mail.misouri.edu>
2015-04-18 16:52:58 -0500

    more capital letter issues

--- php/application/models/instructor_model.php
+++ php/application/models/instructor_model.php
@@ -6,14 +6,14 @@ class instructor_model extends CI_model{
 	}	
 
 	public function get_applicant_data($app){
-		$sql='SELECT * FROM application WHERE username=? LIMIT 1';
+		$sql='SELECT * FROM Application WHERE username=? LIMIT 1';
 		$data=$this->db->query($sql, $app);
 		/*returns an array containing the whole row, should have all of the applicants application data in the order that the db is holding it.*/
 		return $data;
 	}
 
 	public function add_note($input){
-		$sql='INSERT INTO application (note) VALUES (?) WHERE username=?';
+		$sql='INSERT INTO Application (note) VALUES (?) WHERE username=?';
 		/*inserts the note passed in the input array into the note field corresponding to the username also passed in the input array*/
 		$this->db->query($sql,array($input[1],$input[0]));
 	}
@@ -29,7 +29,7 @@ class instructor_model extends CI_model{
     }
 
 	public function get_names(){
-		$sql='SELECT lname,fname FROM application ORDER BY lname';
+		$sql='SELECT lname,fname FROM Application ORDER BY lname';
 		$data=$this->db->query($sql);
 		return $data;
 	}
