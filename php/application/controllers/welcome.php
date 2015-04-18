4e81d33f9eaba96ef58f4b59e3558966b2907c8f
Alex Drake <amd2b7@mail.misouri.edu>
2015-04-18 16:05:40 -0500

    added brackets to the if statement controlling the loading of instrutor view

--- php/application/controllers/welcome.php
+++ php/application/controllers/welcome.php
@@ -28,11 +28,12 @@ class Welcome extends CI_Controller {
 		{
 			if ($this->session->userdata('user_type') == '2')//applicant
 				$this->apply();
-			else if($this->session->userdata('user_type') == '1')//staff
+			else if($this->session->userdata('user_type') == '1'){//staff
 				//$this->load->view('header'); //want to load standard header before instructor view-chantal
 				//needed names from the db before we called the view in order to populate the drop down box
 				$names=$this->get_names();
 				$this->load->view('Instructor_view',$names);
+				}
 			else if($this->session->userdata('user_type') == '0')//admin
 				$this->load->view('admin');
 		}
