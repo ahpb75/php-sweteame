da0fe60d276fe1ef3f74e5ef9eb130bc38b6cd1a
Chantal <cflzr3@mail.missouri.edu>
2015-04-19 11:53:27 -0500

    view changes

--- php/application/views/Instructor_view.php
+++ php/application/views/Instructor_view.php
@@ -2,32 +2,20 @@
 <html>
 <head>
 
-
-
-</head>
-    <title> TA Selection </title>
-
+<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>bootstrap/css/bootstrap.css" />
 <body>
+
 <h1> Computer Science Department </h1>
 <h2> Applicants </h2>
 
+
 <?php
 echo "hi";
-foreach($data->row() as $row){
-    echo $row->lname;
-    echo ", ".$row->fname;
-
-    //echo "hi";
-    //echo $data; 
+ 
 ?>
 
-    <!--<br />;  -->
+   
 
-
-
-    <button onclick="location.href ='Need location of function that pulls out form';" id = "view_button"> View Application</button>
-    <!--Will redirect to another page with application that they can select to leave comments -->
-}
 
 
 </body>
