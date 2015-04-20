fab7d134e1f149309e30201298d8a79da5c2712b
vincentlinuno <vincentlinuno@yahoo.com>
2015-04-19 22:05:54 -0500

    added background to admin home page

--- php/application/views/admin.php
+++ php/application/views/admin.php
@@ -76,75 +76,82 @@
             <p> This is where you can apply, view feedback, and know which courses you've been assigned to.</p>
             <p><a class="btn btn-primary btn-large">Begin Here</a>
             </p>
+            <br>
+            <br>
+            <hr>
+
+            <!-- Title -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h3>Dashboard</h3>
+                </div>
+            </div>
+            <!-- /.row -->
+
+            <!-- Page Features -->
+            <div class="row text-center">
+
+                <div class="col-md-3 col-sm-6 hero-feature">
+                    <div class="thumbnail">
+                        <img src="../../bootstrap/img/ta_app.png" alt="">
+                        <div class="caption">
+                            <h3>View Application Form</h3>
+                            <p>Submit a TA/PLA application for review.</p>
+                            <p>
+                                <?php echo form_open("Admin_controller/view_form1");?>
+                                <input class="btn btn-primary btn-large" type="submit" name="view" value="View" />
+                                <?php echo form_close();?>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 hero-feature">
+                    <div class="thumbnail">
+                        <img src="../../bootstrap/img/create_course.png" alt="">
+                        <div class="caption">
+                            <h3>Create Course <br><br></h3>
+                            <p>Create a course. <br><br></p>
+                            <p>
+                                <?php echo form_open("Admin_controller/create_course");?>
+                                <input class="btn btn-primary btn-large" type="submit" name="view" value="Create" />
+                                <?php echo form_close();?>
+                            </p>
+                            <br>
+                            <br>
+                            <br>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 hero-feature">
+                    <div class="thumbnail">
+                        <img src="../../bootstrap/img/assign_score.png" alt="">
+                        <div class="caption">
+                            <h3>Assign TA/PLA or Enter Score</h3>
+                            <p>Assign them here <br><br></p>
+                            <p>
+                                <?php echo form_open("Admin_controller/assign_ta");?>
+                                <input class="btn btn-primary btn-large" type="submit" name="view" value="Assign" />
+                                <?php echo form_close();?>
+                            </p>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- /.row -->
+
+            <hr>
         </header>
 
-        <hr>
 
-        <!-- Title -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h3>Dashboard</h3>
-            </div>
-        </div>
-        <!-- /.row -->
-
-        <!-- Page Features -->
-        <div class="row text-center">
-
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="../../bootstrap/img/ta_app.png" alt="">
-                    <div class="caption">
-                        <h3>View Application Form</h3>
-                        <p>Submit a TA/PLA application for review.</p>
-                        <p>
-                            <?php echo form_open("Admin_controller/view_form1");?>
-                            <input class="btn btn-primary btn-large" type="submit" name="view" value="View" />
-                            <?php echo form_close();?>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="../../bootstrap/img/create_course.png" alt="">
-                    <div class="caption">
-                        <h3>Create Course <br><br></h3>
-                        <p>Create a course. <br><br></p>
-                        <p>
-                            <?php echo form_open("Admin_controller/create_course");?>
-                            <input class="btn btn-primary btn-large" type="submit" name="view" value="Create" />
-                            <?php echo form_close();?>
-                        </p>
-                        <br>
-                        <br>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="../../bootstrap/img/assign_score.png" alt="">
-                    <div class="caption">
-                        <h3>Assign TA/PLA or Enter Score</h3>
-                        <p>Assign them here <br><br></p>
-                        <p>
-                            <?php echo form_open("Admin_controller/assign_ta");?>
-                            <input class="btn btn-primary btn-large" type="submit" name="view" value="Assign" />
-                            <?php echo form_close();?>                       
-                        </p>
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-        <!-- /.row -->
-
-        <hr>
 
         <!-- Footer -->
         <footer>
