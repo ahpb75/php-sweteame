fdd2abc9c1c7c46f211dc3365e8cab9e87672280
Chantal <cflzr3@mail.missouri.edu>
2015-04-26 14:48:18 -0500

    nvm stashing dropdown helper

--- php/application/views/instructor_choose.php
+++ php/application/views/instructor_choose.php
@@ -18,7 +18,7 @@
  <?php
 
 
- $this->load->helper('dropdown');
+// $this->load->helper('dropdown');
  $this->load->helper('form');
 echo form_open('Instructor_controller/getapp');
 echo form_dropdown('dropdown_menu', $options, '0');
